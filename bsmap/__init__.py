a15e36d9dc5d0a74948edfee03ec039d0f38843d
CodeSoftGit <137560679+CodeSoftGit@users.noreply.github.com>
2025-05-27 18:40:03 -0400

    Bump version to 1.1.2 in __init__.py

--- bsmap/__init__.py
+++ bsmap/__init__.py
@@ -17,7 +17,7 @@ Common Errors:
 """
 
 # Version info
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __author__ = "CodeSoftGit"
 
 # Core models
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bsmap",
-    version="1.1.1",
+    version="1.1.2",
     author="CodeSoftGit",
     description="Beat Saber Mapping Framework",
     long_description=open("README.md", encoding="utf-8").read(),
